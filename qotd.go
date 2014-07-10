928fee273920d70fe349af5c64df740d760e857c
Ryan Sullivan <r.sullivan@modcloth.com>
2014-07-10 13:53:09 -0700

    Run go fmt on the code

--- qotd.go
+++ qotd.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "io/ioutil"
-  "strings"
-  "time"
-  "math/rand"
-  "fmt"
-  "net"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net"
+	"os"
+	"strings"
+	"time"
 )
 
 /* WANT:
@@ -28,29 +28,29 @@ $CDPATH
 */
 
 func main() {
-  rand.Seed(time.Now().UnixNano())
-  l, err := net.Listen("tcp", "localhost:3333")
-  if err != nil {
-    fmt.Println("Error listening:", err.Error())
-      os.Exit(1)
-  }
-  defer l.Close()
-  fmt.Println("Listening")
-  for {
-    conn, err := l.Accept()
-      if err != nil {
-        fmt.Println("Error accepting: ", err.Error())
-          os.Exit(1)
-      }
-    go handleRequest(conn)
-  }
+	rand.Seed(time.Now().UnixNano())
+	l, err := net.Listen("tcp", "localhost:3333")
+	if err != nil {
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
+	}
+	defer l.Close()
+	fmt.Println("Listening")
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+		go handleRequest(conn)
+	}
 }
 
 func handleRequest(conn net.Conn) {
-  quote := randomQuote("wisdom.txt")
-  conn.Write([]byte(quote))
-  conn.Write([]byte("\r\n"))
-  conn.Close()
+	quote := randomQuote("wisdom.txt")
+	conn.Write([]byte(quote))
+	conn.Write([]byte("\r\n"))
+	conn.Close()
 }
 
 func randomQuote(fileName string) string {
@@ -58,7 +58,7 @@ func randomQuote(fileName string) string {
 	if err != nil {
 		panic(err)
 	}
-  quotes := strings.Split(string(file), "\n%\n")
-  randQuoteIndex := rand.Intn(len(quotes))
+	quotes := strings.Split(string(file), "\n%\n")
+	randQuoteIndex := rand.Intn(len(quotes))
 	return quotes[randQuoteIndex]
 }
